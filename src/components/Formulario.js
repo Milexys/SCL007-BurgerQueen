d751186ddc45f65365a1c2822fa9eeb43e413027
Milexys Ferrer <milexys.ferrerp@gmail.com>
2019-03-26 14:12:30 -0300

    instalando redux

--- src/components/Formulario.js
+++ src/components/Formulario.js
@@ -7,14 +7,16 @@ constructor(){
     super();
     this.state = {
         name: "",
+        result: "",
     }
     this.handleData = this.handleData.bind(this);
     this.handleChange = this.handleChange.bind(this);
+    this.handleResult = this.handleResult.bind(this);
 }
     handleChange = (e) => {
         e.preventDefault();
         this.setState({
-            name: this.searchRef.current.value
+            name: this.searchRef.current.value,
         })
     }
     handleData = (e) => {
@@ -22,7 +24,7 @@ constructor(){
         this.setState({
             name: "",
         })
-        db.doc("BurguerQueen/pedidos").set({
+        db.collection("pedidos").add({
             clientName: this.state.name,
             food: this.props.order
         }).then(() => {
@@ -32,29 +34,54 @@ constructor(){
             console.error("error");
         });
     }
-
+    handleResult = (e, index) => {
+        e.preventDefault();
+        this.setState({
+            result: this.props.order.reduce((accum, currentValue)=>{
+                return accum + currentValue.cost
+            }, 0),
+        })
+        
+      
+    }
     render(){
         return (
                 <form onSubmit={this.handleData}>
                     <label>Nombre del Cliente: </label>
                     <input ref={this.searchRef} type="text" className="form-control" onChange={this.handleChange} />
-                            {this.props.order.map((item, i) => {
-                                return (
-                                    <div key={i} className="row">
-                                        <div className="col-md-6">
-                                            {item.name}
-                                        </div>
-                                        <div className="col-md-6">
-                                            ${item.cost}
-                                        </div>
+                    <div className="row">
+                        <div className="col-md-6">
+                            <p className="text-info">Items</p>
+                        </div>
+                        <div className="col-md-4">
+                            <p className="text-info">Precio</p>
+                        </div>
+                        <div className="col-md-2">
+                        </div>
+                    </div>
+                        {this.props.order.map((item, i) => {
+                            return (
+                                <div key={i} className="row">
+                                    <div className="col-md-6">
+                                        {item.name}
+                                    </div>
+                                    <div className="col-md-4">
+                                        ${item.cost}
+                                    </div>
+                                    <div className="col-md-2">
+                                        <a href="#">
+                                            <i className="material-icons">
+                                            cancel
+                                            </i>
+                                        </a>
+                                    </div>
                                 </div>
-                                )
-                            })}
-                        
-                    
-                    
+                            )
+                    })}
+                    <div className="row">
+                        {this.state.result}
+                    </div>
                     <input type="submit" className="btn btn-info mt-2" value="SUBMIT"/>
-                    
                 </form>
         )
     }
